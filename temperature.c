afbf390747dd479c3f54a8ea268e6641ce61a8d6
Matthew Alsup <matthew.alsup@asurion.com>
2019-03-24 13:15:19 -0500

    sec1.4

--- temperature.c
+++ temperature.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 /* print Farenheit-Celsius table for fahr = 0, 20, ..., 300 */
 int main()
 {
 	int fahr;
 
 	printf("F\tC\n");
-	for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+	for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
 		printf("%3d\t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 	return 0;
 }
